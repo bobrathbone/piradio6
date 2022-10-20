27d22e4e6281197459626bc97e1908124927cfb3
Bob Rathbone <bob@bobrathbone.com>
2022-10-20 11:45:01 +0100

    Update to MPD 0.23.9 and Patch 7.4.2
    Version  MPD 0.23.9
    Patch radiod 7.4.2

--- patch.sh
+++ patch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: patch.sh,v 1.28 2022/01/12 09:58:13 bob Exp $
+# $Id: patch.sh,v 1.31 2022/10/20 10:11:31 bob Exp $
 # Patching script for the Raspberry PI radio
 export LC_ALL=C
 
@@ -7,16 +7,16 @@ PKGDEF=piradio
 VERSION=$(grep ^Version: ${PKGDEF} | awk '{print $2}')
 ARCH=$(grep ^Architecture: ${PKGDEF} | awk '{print $2}')
 PATCHLOG=patch.log
-PATCHFILES="translate_class.py playlist_class.py radiod.py display_wifi.sh config_class.py display_config.sh source_class.py radio_class.py"
+PATCHFILES="configure_radio.sh create_stations.py"
 
 sudo chown pi:pi *.py
 sudo chmod +x *.py
 sudo chown pi:pi *.sh
 sudo chmod +x *.sh
-sudo chmod -x language/* voice.dist
+sudo chmod -x language/* voice.dist 
 
-VERSION=7.3
-NUM=4
+VERSION=7.4
+NUM=2
 # Tar build files
 PATCHTAR="radiod-patch-${VERSION}-${NUM}.tar.gz" 
 echo "Creating patch ${PATCHTAR} $(date)" | tee ${PATCHLOG}
